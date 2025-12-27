945cdac1d0203c956c112dd6aea025f1510c8d28
mosahel <sahel.70444@gmail.com>
2025-12-28 03:30:57 +0530

    Update WindowedAttention.cpp

--- CPP1/builder/WindowedAttention.cpp
+++ CPP1/builder/WindowedAttention.cpp
@@ -1,168 +1,132 @@
 #include<iostream>
 #include"stream.hpp"
 
+#define DEBUG_MODE 0;
 
-template<unsigned int SEQ_LEN, unsigned int HDIM, unsigned int SIMD, unsigned int WIN_SIZE = 256, unsigned int WIN_SIZE_RIGHT = 128, unsigned int WIN_SIZE_LEFT = 127> // First with just plain attention no packed
 
-class WindowedAttention
+template<typename T, unsigned int SEQ_LEN, unsigned int HDIM, unsigned int SIMD, unsigned int WIN_SIZE = 256, unsigned int WIN_SIZE_LEFT = 127, unsigned int WIN_SIZE_RIGHT = 128>
+
+class WindowedAttentionEngine
 {
     public:
         
-        hls_stream<int,1>& query_stream;
-        hls_stream<int,1>& key_stream;
-        hls_stream<int,1>& out_stream;
+        hls_stream<T,1>& Q_stream;
+        hls_stream<T,1>& K_stream;
+        hls_stream<T,1>& O_stream;
 
+        WindowedAttentionEngine(hls_stream<T,1>& QS, hls_stream<T,1>& KS, hls_stream<T,1>& OS)
+        :Q_stream{QS},
+        K_stream{KS},
+        O_stream{OS}
+        {
+        
+        }
 
+        static_assert(HDIM % SIMD == 0, "HDIM MUST BE DIVISIBLE BY SIMD");
 
-        WindowedAttention(hls_stream<int,1>& QS, hls_stream<int,1>& KS, hls_stream<int,1>& OS)
-        : query_stream{QS},
-         key_stream{KS},
-         out_stream{OS}
-         {
-
-         }
-
-    // SIMD ---> number of multipliers parallely reading data from a head (Must be HDIM % SIMD == 0)
-    // I have to store KEYS right?????
-        // constexpr unsigned int total_k_size = HDIM * (WIN_SIZE + 1); 
-        int k_buf[WIN_SIZE+1][HDIM]; //+1 for buffering
-        int q_buf[2][HDIM];
+        T K_buf[WIN_SIZE+1][HDIM]; //+1 to enable simulatneously reading and writing keys
+        T Q_buf[2][HDIM]; // Ping-pong buffer design
         // #pragma ARRAY_PARTITION_ var = k_buf type = cyclic  factor = SIMD  
         // #pragma ARRAY_PARTITION_ var = q_buf type = cyclic  factor = SIMD 
 
-        // constexpr unsigned int total_v_size = HDIM * (WIN_SIZE+1);
-        
-        // const unsigned int calculate_total_ops = [](unsigned int seq_len = SEQ_LEN, unsigned int win_size_R = WIN_SIZE_RIGHT, unsigned int win_size_L = WIN_SIZE_LEFT){
 
-        //     unsigned int total_ops{};
-        //     for(int i = 0; i < seq_len; i++)
-        //     {
-        //       unsigned int  start_index = std::max(0, static_cast<int>(i - win_size_L) );
-        //       unsigned int end_index = std::min(seq_len, i + win_size_R + 1);
-        //       unsigned int valid_keys = end_index - start_index;
-        //       total_ops += valid_keys;
+         static constexpr unsigned int total_ops = [](unsigned int seq_len = SEQ_LEN, unsigned int win_size_R = WIN_SIZE_RIGHT, unsigned int win_size_L = WIN_SIZE_LEFT) constexpr {
+
+            unsigned int total_ops{};
+            for(int i = 0; i < seq_len; i++)
+            {
+            int start = (i - (int)WIN_SIZE_LEFT < 0) ? 0 : (i - WIN_SIZE_LEFT);
+            int end   = (i + WIN_SIZE_RIGHT + 1 > (int)SEQ_LEN) ? SEQ_LEN : (i + WIN_SIZE_RIGHT + 1);
+              unsigned int valid_keys = end - start;
+              total_ops += valid_keys;
                 
-        //     }
+            }
 
-        //     return total_ops;
-        // };
-        // const unsigned int read_cycles = HDIM; // read cycles to load a row
-        // const unsigned int multiplying_factor = HDIM/SIMD; // Cycles to write one attention score between heads of one query and key ALWAYS MORE THAN read_cycles
-        const unsigned int write_cycles = multiplying_factor; //FOr now assume its int 
-        // const unsigned int fill_full_buffer = total_k_size; 
-        
-        // const unsigned int free_flow_start_cycle = read_cycles * WIN_SIZE_RIGHT; // after this we have plenty of time to read because the new query starts from the begenning
+            return total_ops;
 
-        // static constexpr unsigned int Total_iter = (calculate_total_ops() * multiplying_factor) + read_cycles;        Total_iter *=multiplying_factor 
+        }();
 
+        static constexpr int compute_factor = HDIM/SIMD; //ASSUMING II=1 !!
+        static constexpr unsigned int write_cycles = compute_factor;
+        static constexpr unsigned int read_cycles = HDIM; //should add SIMD packets if any
 
-        static constexpr unsigned int calculate_total_ops() {
-        unsigned int total_ops = 0;
-        for(unsigned int i = 0; i < SEQ_LEN; i++) {
-            // Use signed int for the subtraction to avoid underflow before max()
-            int start_idx = ( (int)i - (int)WIN_SIZE_LEFT > 0 ) ? (i - WIN_SIZE_LEFT) : 0;
-            unsigned int end_index = (i + WIN_SIZE_RIGHT + 1 < SEQ_LEN) ? (i + WIN_SIZE_RIGHT + 1) : SEQ_LEN;
-            total_ops += (end_index - (unsigned int)start_idx);
-        }
-        return total_ops;
-    }
+        static constexpr unsigned int total_iter = (total_ops * compute_factor) + read_cycles + 10000;
 
-    // 2. Define constants correctly
-    static constexpr unsigned int multiplying_factor = HDIM / SIMD;
-    static constexpr unsigned int read_cycles = HDIM;
-    
-    // 3. Initialize Total_iter using the helper
-    static constexpr unsigned int Total_iter = (calculate_total_ops() * multiplying_factor) + read_cycles;
-        
-        int attn_scores[WIN_SIZE];
+        T attn_scores[WIN_SIZE+1];
 
-
-        
-        void attn_function()
+        void attn()
         {
+            //flags
+            bool unrestricted_compute = false; //Once set true will compute unconditionally
+            bool load_k_enable = true; // Will be set false once the window is full
+            bool load_q_enable = true; // decides when to load the (n+1)th query
+            bool active_q_load = false;
+            bool active_k_load = false;
 
-            bool free_flow_start = false;
-            bool load_enable = true;
-            bool load_q_enable = true;
-        
-
+            //K-buffer pointers
             unsigned int kwx = 0;
             unsigned int kwy = 0;
             unsigned int krx = 0;
-            unsigned int kry = 0; //SOME POINTER
-            
-            unsigned int qrx = 0;
-            
+            unsigned int kry = 0;
+
+            // Q-buffer pointers
             unsigned int qwx = 0;
             bool qwy = 0;
+            unsigned int qrx = 0;
             bool qry = 0;
+
+            unsigned int ky_idx = 0; // kry + key_start - points from the start key in the K-buffer till the end
+
             
-
-            unsigned int ky_index = 0; // kry + key_start
-          
-
-
-            // unsigned int simd_count = 0; //----> For counting how much of input is processed
-
-            unsigned int key_start = 0;// Where does a query start reading from 
-            unsigned int write_dest = 0;
+            unsigned int key_start = 0;// start of initial k-row in buffer
             unsigned int key_row_count = 0;
-            unsigned int keys_processed_count = 0;
             unsigned int query_row_count = 0;
-            unsigned int internal_counter = 0; 
+            unsigned int internal_counter = 0;
+            unsigned int query_load_count = 0;
+            unsigned int initial_read_count = 0;
+
+            unsigned int k_read_start = 0;
+            unsigned int k_read_end = WIN_SIZE_RIGHT+1;
+
+            T acc_value = 0;
+            unsigned int attn_score_counter = 0;
 
 
-            unsigned int inp = 0; // Counts upto first read_cycles to fill the very first Q and K
-
-            unsigned int finish_read = 0;
-            unsigned int read_till = WIN_SIZE_RIGHT;
-            int acc_value = 0;
-
-
-            unsigned int attn_score_counter  = 0;
-
-
-            for(int i = 0; i < 10000; i++) //TOMORROW : THIS LOOP COUNT GAVE SEG FAULT
+            for(int i = 0; i < total_iter; i++)
             {
-
-                if(inp < read_cycles)
+                if(initial_read_count < read_cycles)
                 {
-                     int q_val = query_stream.read();
-                     int k_val = key_stream.read();
+                    T q_val = Q_stream.read();
+                    T k_val = K_stream.read();
 
-                    k_buf[kwy][kwx] = k_val;
-                    q_buf[qwy][qwx] = q_val;
+                    K_buf[kwy][kwx] = k_val;
+                    Q_buf[qwy][qwx] = q_val;
 
                     kwx++;
                     qwx++;
-                    inp++; 
+                    initial_read_count++;
 
-                    if(inp == read_cycles)
+                    if(initial_read_count==read_cycles)
                     {
                         kwx = 0;
-                        kwy++; 
                         qwx = 0;
+                        kwy++;
                         qwy = !qwy;
-                        key_row_count++; // FIRST ROW READDD
+                        key_row_count++;
+                        query_load_count++;
                     }
-                    
                 }
 
                 else
                 {
-                    //Case1: layer is still loading but we compute N-1th row which was previously fetched while loading Nth row
-                    //Case2: free flow we restarted the computation
-                    //Case3: Full buffer is filled !!!
-
-                    if(internal_counter < write_cycles || free_flow_start)
+                    if(internal_counter < write_cycles || unrestricted_compute)
                     {
-
                         for(int sd = 0; sd < SIMD; sd++)
-                        //pragma UNROLL
+                        //#PRAGMA UNROLL --> HERE
                         {
-                            acc_value += q_buf[qry][qrx+sd] * k_buf[ky_index][krx+sd];
-
+                            acc_value += Q_buf[qry][qrx+sd] * K_buf[ky_idx][krx+sd];
                         }
+
                         qrx += SIMD;
                         krx += SIMD;
 
@@ -170,120 +134,150 @@ class WindowedAttention
                         {
                             qrx = 0;
                             krx = 0;
+                            unsigned int attn_score_width = k_read_end - k_read_start;
+
                             attn_scores[attn_score_counter] = acc_value;
                             acc_value = 0;
+
                             attn_score_counter++;
-                            keys_processed_count++;
                             kry++;
-                            ky_index = key_start + kry;
 
-                            if(ky_index >= WIN_SIZE+1)
+                            ky_idx = key_start + kry;
+
+
+                            if(ky_idx >= WIN_SIZE+1)
                             {
-                                ky_index -= WIN_SIZE+1;
+                                ky_idx -= WIN_SIZE+1;
                             }
-                            // finish_read++;
 
-                            if(attn_score_counter == read_till - key_start)
+
+                            if(attn_score_counter == attn_score_width)
                             {
-                                free_flow_start = true;
+                                unrestricted_compute = true;
 
-                                if(load_enable == false)
+                                if(load_q_enable == false)
                                 {
-                                    load_enable = true;
                                     load_q_enable = true;
                                 }
-                                // finish_read = 0;
-                                read_till++;
-                                
-                                query_row_count++;
-                                qry = !qry;
-                                keys_processed_count = 0;
 
-                                if(read_till == SEQ_LEN)
+                                if(query_row_count > WIN_SIZE_LEFT)
                                 {
-                                    read_till = SEQ_LEN-1;
+                                    load_k_enable = true;
                                 }
 
-                                if(query_row_count > WIN_SIZE_LEFT) //same as >= WIN_SIZE_LEFT+1
+                                k_read_end++;
+                                query_row_count++;
+                                qry = !qry;
+                                kry = 0;
+
+                                if(k_read_end > SEQ_LEN)
                                 {
+                                    k_read_end = SEQ_LEN;
+                                }
+
+                                if(query_row_count > WIN_SIZE_LEFT)
+                                {
+                                    k_read_start++;
                                     key_start++;
 
-                                    if(key_start >= WIN_SIZE+1)
+                                    if(key_start == WIN_SIZE+1)
                                     {
                                         key_start = 0;
                                     }
                                 }
 
-                                for(int pr = 0; pr < attn_score_counter ; pr++)
+                                ky_idx = key_start;
+
+
+                                // ONLY FOR DEBUGGING - WILL REMOVE IT LATER:
+                                for(int ro = 0; ro < attn_score_counter; ro++)
                                 {
-                                    out_stream.write(attn_scores[pr]); // THIS IS CAUSING PROBLEMS I FEEL
+                                    O_stream.write(attn_scores[ro]);
                                 }
-                                attn_score_counter = 0;
-                                
+
+                                attn_score_counter = 0; // MOVE IT EARLIER PROBABLY
                             }
+
+                            
                         }
                     }
 
-                    if(internal_counter < read_cycles && load_enable) // ONE ROW FULL FILL
-                    {
-                        int k_val = key_stream.read();
-                        k_buf[kwy][kwx] = k_val;
-                        kwx++;
 
-                        if(load_q_enable)
+                    if(internal_counter < read_cycles)
+                    {
+
+                        if(load_k_enable && internal_counter == 0 && key_row_count < SEQ_LEN)
                         {
-                            int q_val = query_stream.read();
-                            q_buf[qwy][qwx] = q_val;
+                            active_k_load = true;
+                        }
+
+                        if(active_k_load)
+                        {
+
+                            T k_val = K_stream.read();
+                            K_buf[kwy][kwx] = k_val;
+                            kwx++;
+                        }
+
+                        if(load_q_enable && internal_counter==0 && query_load_count < SEQ_LEN)
+                        {
+                            active_q_load = true;
+                        }
+
+                        if(active_q_load)
+                        {
+                            T q_val = Q_stream.read();
+                            Q_buf[qwy][qwx] = q_val;
                             qwx++;
                         }
 
-
-
                         if(internal_counter == read_cycles-1)
                         {
-                            
-                            kwx = 0;
-                            kwy++;
-                            key_row_count++;
-                           
-                            if(load_q_enable)
+                            if(active_k_load)
                             {
+                                kwx = 0;
+                                kwy++;
+                                key_row_count++;
+                                active_k_load = false;
+
+                                if(key_row_count >= WIN_SIZE+1)
+                                {
+                                    load_k_enable = false;
+                                }
+
+                                if(kwy == WIN_SIZE+1)
+                                {
+                                    kwy = 0;
+                                }
+                            }
+
+                            if(active_q_load)
+                            {
+                                query_load_count++;
                                 qwx = 0;
                                 qwy = !qwy;
-                                load_q_enable = false; //TODO will be set true in compute engine
+                                load_q_enable = false;
+                                active_q_load = false;
+
                             }
-
-                            if(key_row_count >= WIN_SIZE+1)
-                            {   
-                                
-                                load_enable = false; //TODO once set false will be set true in the compute engine
-                            }
-
-                            if(kwy == WIN_SIZE+1)
-                            {
-                                kwy -= WIN_SIZE+1;
-                            }
-
-
+                            
                         }
-
-
-                    }
-
-                    internal_counter++;
-
-                    if(internal_counter==read_cycles)
-                    {
-                        internal_counter = 0;
                     }
 
 
-             }
+
+                }
+
+            internal_counter++;
+
+            if(internal_counter == read_cycles)
+            {
+                internal_counter = 0;
+            }
+            }
+
 
         }
 
-    }
+
 };
-
-
-//ONLY READ
